ec1947d2e677347f937e8dc3c17be46424d4df11
Alberto Valenzuela <avalen28@gmail.com>
2023-03-26 19:25:05 +0200

    ✨Adds Inventary model

---
+++ models/Inventary.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const inventarySchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: User,
+      required: true,
+    },
+    food: {
+      type: Boolean,
+      default: false,
+    },
+    drinks: {
+      type: Boolean,
+      default: false,
+    },
+    sportswear: {
+      type: Boolean,
+      default: false,
+    },
+    footwear: {
+      type: Boolean,
+      default: false,
+    },
+    other: [String],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Inventary = model("Inventary", inventarySchema);
+module.exports = Inventary;
