1eba40fbcb1891bf79335fb68537f57f7345fc39
Alberto Valenzuela <avalen28@gmail.com>
2023-03-26 18:26:57 +0200

    ✨ Edits User model

--- models/User.js
+++ models/User.js
@@ -1,28 +1,46 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  hashedPassword: {
-    type: String,
-    required: true
-  },
-  username: {
-    type: String,
-    required: true
-  },
-  role: {
-    type: String,
-    enum: ['user', 'admin'],
-    default: 'user'
-  }
-},
+const userSchema = new Schema(
   {
-    timestamps: true
-  });
+    username: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      default:
+        "https://img.favpng.com/17/1/20/user-interface-design-computer-icons-default-png-favpng-A0tt8aVzdqP30RjwFGhjNABpm.jpg",
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    hashedPassword: {
+      type: String,
+      required: true,
+    },
+    experiencePoints: {
+      type: Number,
+      min: 0,
+      default: 0,
+    },
+    level: {
+      type: Number,
+      min: 1,
+      max: 5,
+      default: 1,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const User = model("User", userSchema);
 
-module.exports = model("User", userSchema);
+module.exports = User;
