a60bdcd5a8399cf0cd2abd4d3ecd23f3889601b7
Alberto Valenzuela <avalen28@gmail.com>
2023-03-26 20:13:43 +0200

    ✨ Adds user Seed

--- seed/index.js
+++ seed/index.js
@@ -1,21 +1,47 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-// Import the model
+require("dotenv").config();
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+const User = require("../models/User");
 
 // Place the array you want to seed
 
-mongoose.connect(process.env.MONGO_URL)
-  .then(x => console.log(`Connected to ${x.connection.name}`))
-  .then(() => {
-    return // Model.create(array)
-  })
-  .then(() => {
-    console.log('Seed done 🌱');
-  })
-  .catch(e => console.log(e))
-  .finally(() => {
-    console.log('Closing connection');
-    mongoose.connection.close();
-  })
+//keys with value "default" is not necessary include it
+const usersArr = [
+  {
+    username: "Bojack Horseman",
+    email: "bojack@gmail.com",
+    hashedPassword: "fj<lkfj3lkjfl", //password hardcode hashed
+  },
+  {
+    username: "Ale",
+    email: "Ale@gmail.com",
+    hashedPassword: "fj<lkfj3lkjfl", //password hardcode hashed
+  },
+  {
+    username: "Marina",
+    email: "Marina@gmail.com",
+    hashedPassword: "fj<lkfj3lkjfl", //password hardcode hashed
+  },
+];
 
-// Run npm run seed 
+
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then((x) => console.log(`Connected to ${x.connection.name}`))
+  .then(() => {
+    return User.deleteMany({});
+  })
+  .then(() => {
+    return User.create(usersArr);
+  })
+  .then((created) => {
+    console.log(`Created ${created.length} Users Seed done 🌱`);
+  })
+  .catch((e) => console.log("error connecting Mongo", e))
+  .finally(() => {
+    console.log("Closing connection");
+    mongoose.connection.close();
+  });
+
+// Run npm run seed
