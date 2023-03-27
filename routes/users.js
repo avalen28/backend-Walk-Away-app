e1f2093c1b203d6385aad719a6e76f24b52b8cc8
Alberto Valenzuela <avalen28@gmail.com>
2023-03-27 19:24:26 +0200

    xl<zmx<x :q r :r adds first validations. WIP

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const router = require("express").Router()
 const User = require("../models/User")
 const Inventary = require("../models/Inventary")
+const bcrypt = require("bcryptjs");
+const saltRounds = 10;
 
 // @desc    Get all Users
 // @route   GET /users/all
@@ -27,14 +29,42 @@ router.get("/:userId", async (req, res, next) => {
   }
 });
 
-// @desc    PUT edit User
+// @desc    PUT edit Users
 // @route   PUT /users/edit/:userId
 // @access  Public
 router.put("/edit/:userId", async (req, res, next) => {
-    const { userId } = req.params;
+  const { userId } = req.params;
+  const { username, email, password1, password2 } = req.body;
+
+  //Check all fields on edit body are filled
+  if (username === "" || email === "" || password1 === "" || password2 === "") {
+    res.status(400).json({ message: "Please fill all the fields to edit" });
+    return;
+  }
+  // Use regex to validate the email format
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+  if (!emailRegex.test(email)) {
+    res.status(400).json({ message: "Not a valid email format" });
+    return;
+  }
+// Check if both passwords are the same
+  if (password1 !== password2) {
+    res.status(400).json({ message: "Please check your password" });
+    return;
+  }
+  // Use regex to validate the password format
+  const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!passwordRegex.test(password1)) {
+    res.status(400).json({
+      message:
+        "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter",
+    });
+    return;
+  }
+
   try {
-      await User.findByIdAndUpdate(userId, req.body, { new: true });
-      const updateUser = await User.findById(userId)
+    await User.findByIdAndUpdate(userId, req.body, { new: true });
+    const updateUser = await User.findById(userId);
     res.status(200).json(updateUser);
   } catch (error) {
     next(error);
