338254b28d983203d4ce1a72ae250285d154a3fb
Alberto Valenzuela <avalen28@gmail.com>
2023-04-02 14:44:32 +0200

    adds edit logic

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-const router = require("express").Router()
-const User = require("../models/User")
-const Inventary = require("../models/Inventary")
+const router = require("express").Router();
+const User = require("../models/User");
+const Inventary = require("../models/Inventary");
 const { isAuthenticated, isAdmin } = require("../middlewares/jwt");
 const bcrypt = require("bcryptjs");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 const isValid = require("../utils/index");
 
 // @desc    Get all Users
@@ -23,8 +24,8 @@ router.get("/all", isAuthenticated, isAdmin, async (req, res, next) => {
 // @route   GET /users/me
 // @access  Private
 // @Postman Checked
-router.get("/me",isAuthenticated, async (req, res, next) => {
-    const { _id } = req.payload;
+router.get("/me", isAuthenticated, async (req, res, next) => {
+  const { _id } = req.payload;
   try {
     const user = await User.findById(_id);
     res.status(200).json(user);
@@ -37,7 +38,8 @@ router.get("/me",isAuthenticated, async (req, res, next) => {
 // @route   PUT /users/edit/
 // @access  Private
 // @Postman Checked
-router.put("/edit",isAuthenticated, async (req, res, next) => {
+router.put("/edit", isAuthenticated, async (req, res, next) => {
+ 
   const { _id } = req.payload;
   const { username, email, img, password1, password2 } = req.body;
 
@@ -45,10 +47,10 @@ router.put("/edit",isAuthenticated, async (req, res, next) => {
     res.status(400).json({ message: "Please provide a valid user name" });
     return;
   }
-    if (!isValid(img, "string")) {
-      res.status(400).json({ message: "Please provide a valid url image" });
-      return;
-    }
+  if (!isValid(img, "string")) {
+    res.status(400).json({ message: "Please provide a valid url image" });
+    return;
+  }
   if (!isValid(email, "email")) {
     res.status(400).json({ message: "Not a valid email format" });
     return;
@@ -59,23 +61,36 @@ router.put("/edit",isAuthenticated, async (req, res, next) => {
       res.status(400).json({ message: "Please check both passwords" });
       return;
     }
-      if (!isValid(password1, "password")) {
-        res.status(400).json({
-          message:
-            "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter",
-        });
-        return;
-      }
-        const salt = bcrypt.genSaltSync(saltRounds);
-        const hashedPassword = bcrypt.hashSync(password1, salt);
+    if (!isValid(password1, "password")) {
+      res.status(400).json({
+        message:
+          "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter",
+      });
+      return;
+    }
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password1, salt);
     req.body.hashedPassword = hashedPassword;
-
   }
-  
-  try {
 
-    const updateUser = await User.findByIdAndUpdate(_id, req.body, { new: true });
-    res.status(200).json(updateUser);
+  try {
+    const updatedUser = await User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+    });
+     const payload = {
+       username: updatedUser.username,
+       img: updatedUser.img,
+       email: updatedUser.email,
+       experiencePoints: updatedUser.experiencePoints,
+       level: updatedUser.level,
+       isAdmin: updatedUser.isAdmin,
+       _id: updatedUser._id,
+     };
+     const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+       algorithm: "HS256",
+       expiresIn: "30d",
+     });
+    res.status(200).json({updatedUser,authToken});
   } catch (error) {
     next(error);
   }
@@ -85,18 +100,16 @@ router.put("/edit",isAuthenticated, async (req, res, next) => {
 // @route   DELETE /users/delete
 // @access  Private
 // @Postman Checked
-router.delete("/delete",isAuthenticated, async (req, res, next) => {
-    const { _id } = req.payload;
+router.delete("/delete", isAuthenticated, async (req, res, next) => {
+  const { _id } = req.payload;
   try {
-    await Inventary.findOneAndDelete({ userId:_id });
-      await User.findByIdAndDelete(_id);
-      const allUsers = await User.find()
-    res.status(200).json({message: "user deleted"});
+    await Inventary.findOneAndDelete({ userId: _id });
+    await User.findByIdAndDelete(_id);
+    const allUsers = await User.find();
+    res.status(200).json({ message: "user deleted" });
   } catch (error) {
     next(error);
   }
 });
-
-
 
 module.exports = router;
